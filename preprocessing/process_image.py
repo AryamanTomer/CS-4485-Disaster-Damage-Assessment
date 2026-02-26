7dd6a68b22554a727c61fac8f4beab20b39d85d1
AarejSyed <159865376+AarejSyed@users.noreply.github.com>
2026-02-26 11:53:35 -0600

    Image processing script can retrieve one particular image pair and its JSON labels

---
+++ preprocessing/process_image.py
@@ -0,0 +1,33 @@
+from PIL import Image
+import json
+from pathlib import Path
+
+# Project root
+root = Path(__file__).parent.parent
+
+# Image pair name (root of image and label filenames)
+image_pair = "socal-fire_00000000_"
+
+# Directories for:
+images = root / "test_images_labels_targets/test/images/"   # Images
+labels = root / "test_images_labels_targets/test/labels/"   # Labels
+
+# Open image files:
+img_pre_disaster = Image.open(images / (image_pair + "pre_disaster.png"))       # Pre-disaster
+img_post_disaster = Image.open(images / (image_pair + "post_disaster.png"))     # Post-disaster
+
+# Open label files:
+label_pre_disaster_file = open(labels / (image_pair + "pre_disaster.json"), 'r')    # Pre-disaster
+label_post_disaster_file = open(labels / (image_pair + "post_disaster.json"), 'r')  # Post-disaster
+
+# Load label JSONs:
+label_pre_disaster = json.load(label_pre_disaster_file)     # Pre-disaster
+label_post_disaster = json.load(label_post_disaster_file)   # Post-disaster
+
+# Display images (test)
+img_pre_disaster.show()
+img_post_disaster.show()
+
+# Print JSONS (test)
+print(json.dumps(label_pre_disaster, indent=4))
+print(json.dumps(label_post_disaster, indent=4))
