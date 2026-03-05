1e0d06b8c91cd76a177b28cade1b7482660ebc5a
AarejSyed <159865376+AarejSyed@users.noreply.github.com>
2026-03-05 12:03:17 -0600

    Simplified process of polygon --> bounding box conversion

--- preprocessing/process_feature.py
+++ preprocessing/process_feature.py
@@ -39,47 +39,27 @@ class ProcessedFeature:
         # Shapes of processed features are defined by opposing vertices: vertex 1 and vertex 2
 
 
-# Converts POLYGON string from JSON into list of vertices
-def polygon_string_to_list(polygon):
-    # Remove prefix and suffix so polygon string is only comma-separated list of ordered pairs
+# Simplifies feature polygon into smallest axis-aligned bounding box
+def polygon_to_bbox(polygon):
+    # Reduce polygon string to comma-separated list of ordered pairs
     polygon = polygon.removeprefix("POLYGON ((").removesuffix("))")
 
-    # Tokenize polygon: each token is a string representing a ordered pair
-    vertex_strings = polygon.split(", ")
+    # Vertices of bounding box:
+    min_x = min_y = float("inf")    # Min vertex
+    max_x = max_y = float("-inf")   # Max vertex
 
-    # Convert ordered pair strings to actual ordered pair tuples
-    vertices = []
-    for string in vertex_strings:
-        # Tokenize ordered pair string into x and y strings
-        vertex_string_list = string.split(" ")
+    # For each vertex in polygon string:
+    for vertex in polygon.split(","):
+        # Maps vertex x and y from string to float
+        x, y = map(float, vertex.split())
 
-        # Convert x and y strings to floats and add ordered pair to vertices list
-        vertices.append((float(vertex_string_list[0]), float(vertex_string_list[1])))
+        # Updates bounding box vertices:
+        min_x = min(min_x, x)   # Min x
+        min_y = min(min_y, y)   # Min y
+        max_x = max(max_x, x)   # Max x
+        max_y = max(max_y, y)   # Max y
 
-    # Return list of vertices
-    return vertices
-
-
-# Simplifies feature polygon into smallest axis-aligned bounding box
-# Polygon: list of n ordered pairs
-# Bounding box: two ordered pairs at opposing corners of box
-def simplify_feature_shape(feature):
-    # Gets vertices from provided feature
-    vertices = polygon_string_to_list(feature["wkt"])
-
-    # Will store vertices of bounding box; initialized to x and y of first vertex
-    min_x = max_x = vertices[0][0]  # Min x and max x
-    min_y = max_y = vertices[0][1]  # Min y and max y
-
-    # Go through each vertex of polygon to determine bounding box vertices
-    for vertex in vertices:
-        # Find:
-        min_x = vertex[0] if vertex[0] < min_x else min_x   # Min x
-        min_y = vertex[1] if vertex[1] < min_y else min_y   # Min y
-        max_x = vertex[0] if vertex[0] > max_x else max_x   # Max x
-        max_y = vertex[1] if vertex[1] > max_y else max_y   # Max y
-
-    # Return vertices of bounding box as a pair of tuples; min values rounded down and max values rounded up
+    # Returns vertices of bounding box
     return (math.floor(min_x), math.floor(min_y)), (math.ceil(max_x), math.ceil(max_y))
 
 
@@ -99,7 +79,7 @@ def get_damage_class(feature):
 # Process a feature so it is easier to work with
 def process_feature(feature):
     # Get vertices of bounding box from polygon vertices of feature
-    vertex_1, vertex_2 = simplify_feature_shape(feature)
+    vertex_1, vertex_2 = polygon_to_bbox(feature["wkt"])
 
     # Get damage class
     damage_class = get_damage_class(feature)
