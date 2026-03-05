349d38db1727c7f4c95b4b31af760099632be04e
AarejSyed <159865376+AarejSyed@users.noreply.github.com>
2026-03-05 11:20:11 -0600

    Added damage classes to processed features

--- preprocessing/process_feature.py
+++ preprocessing/process_feature.py
@@ -1,15 +1,38 @@
 import math
+from enum import Enum
+
+
+# Enum that represents damage classes
+class DamageClass(Enum):
+    # Damage classes:
+    PRE_DISASTER = 0    # N/A: pre-disaster
+    NO_DAMAGE = 1       # No damage
+    MINOR_DAMAGE = 2    # Minor damage
+    MAJOR_DAMAGE = 3    # Major damage
+    DESTROYED = 4       # Destroyed
+
+
+# Dictionary mapping JSON damage subtype strings to DamageClass enums
+DAMAGE_CLASS_MAP = {
+    # Damage classes:
+    None: DamageClass.PRE_DISASTER,             # None (pre-disaster)
+    "no-damage": DamageClass.NO_DAMAGE,         # No damage
+    "minor-damage": DamageClass.MINOR_DAMAGE,   # Minor damage
+    "major-damage": DamageClass.MAJOR_DAMAGE,   # Major damage
+    "destroyed": DamageClass.DESTROYED          # Destroyed
+}
 
 
 # Defines "processed features": features from the JSON files processed for maximum utility in program
 class ProcessedFeature:
     # ProcessedFeature constructor
-    def __init__(self, uid, feature_type, vertex_1, vertex_2):
+    def __init__(self, uid, feature_type, vertex_1, vertex_2, damage_class):
         # Attributes:
         self.uid = uid                      # UID
         self.feature_type = feature_type    # Type
         self.vertex_1 = vertex_1            # Vertex 1
         self.vertex_2 = vertex_2            # Vertex 2
+        self.damage_class = damage_class    # Damage class
 
         # Unprocessed features have polygonal shapes with n vertices
         # To aid in generating images of features, processed features are rectangular
@@ -60,10 +83,29 @@ def simplify_feature_shape(feature):
     return (math.floor(min_x), math.floor(min_y)), (math.ceil(max_x), math.ceil(max_y))
 
 
+# Gets damage class from JSON feature
+def get_damage_class(feature):
+    # Gets subtype string (not defined if pre-disaster)
+    subtype = feature["properties"].get("subtype")
+
+    try:
+        # If damage subtype string is valid, returns corresponding damage class enum
+        return DAMAGE_CLASS_MAP[subtype]
+    except KeyError:
+        # If damage subtype string is invalid, raises value error
+        raise ValueError(f"Unknown damage subtype: {subtype}")
+
+
 # Process a feature so it is easier to work with
 def process_feature(feature):
     # Get vertices of bounding box from polygon vertices of feature
     vertex_1, vertex_2 = simplify_feature_shape(feature)
 
+    # Get damage class
+    damage_class = get_damage_class(feature)
+
     # Return processed feature
-    return ProcessedFeature(feature["properties"]["uid"], feature["properties"]["feature_type"], vertex_1, vertex_2)
+    return ProcessedFeature(feature["properties"]["uid"],           # UID
+                            feature["properties"]["feature_type"],  # Feature type
+                            vertex_1, vertex_2,                     # Bounds
+                            damage_class)                           # Damage class
